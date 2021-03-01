907a104d8828826affa8d6c3ce7aa3ecccbbea09
Alex <mendelberg44@gmail.com>
2021-03-01 12:20:14 +0200

    filter | show | middle age

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from collections import namedtuple
 Notes = namedtuple("Notes", "code name position subcharpet exp money")
 
 
-def put_data(file_name, staff):
-    with open(f"data/{file_name}.txt", "w") as file:
+def put_data(staff):
+    with open(f"data/notes.txt", "w") as file:
         for worker in staff:
             file.write(
                 f'{worker.code}, {worker.name}, {worker.position}, '
@@ -13,36 +13,54 @@ def put_data(file_name, staff):
 
 
 def input_note():
-    put_data('notes', [
-        Notes(
-            input(f'The person #{_ + 1}\nCode: '),
-            input('Name: '),
-            input('Position: '),
-            input('Subcharpet: '),
-            input('Experience: '),
-            input('Money: ')
-        )
-        for _ in range(
-            int(input('Amount of staff: '))
-        )
-    ])
-
-
-def get_arr_notes(file_name):
-    with open(f'data/{file_name}.txt', 'r') as file:
-        return [
-            Notes(
-                code=worker[0],
-                name=worker[1],
-                position=worker[2],
-                subcharpet=worker[3],
-                exp=worker[4],
-                money=int(worker[5].replace('\n', ''))
-            )
-            for worker in [
-                file.readline().split(', ')
-                for _ in range(
-                    sum(1 for _ in open(f'data/{file_name}.txt', 'r'))
+    put_data([
+                Notes(
+                    input(f'\nThe person #{_ + 1}\nCode: '),
+                    input('Name: '),
+                    input('Position: '),
+                    input('Subcharpet: '),
+                    input('Experience: '),
+                    input('Money: ')
                 )
-            ]
-        ]
+                for _ in range(
+                    int(input('Amount of staff: '))
+                )
+            ])
+
+
+def get_arr_notes():
+    with open('data/notes.txt', 'r') as file:
+        return [
+                    Notes(
+                        code=worker[0],
+                        name=worker[1],
+                        position=worker[2],
+                        subcharpet=worker[3],
+                        exp=worker[4],
+                        money=int(worker[5].replace('\n', ''))
+                    )
+                    for worker in [
+                        file.readline().split(', ')
+                        for _ in range(
+                            sum(1 for _ in open(f'data/notes.txt', 'r'))
+                        )
+                    ]
+                ]
+
+
+def get_notes_filter(subcharpet):
+    workers = [worker for worker in get_arr_notes() if worker.subcharpet == subcharpet]
+    return workers if len(workers) != 0 else 0
+
+
+def show_notes_filter(filtered_notes):
+    if filtered_notes:
+        print(*[worker.name for worker in filtered_notes], sep='\n')
+    else:
+        print('Error!\nThis subcharpet does not exists.')
+
+
+# you need to put 'year' into Notes to working this func!!!
+def get_middle_age_staff(staff):
+    from datetime import datetime as date
+    return date.now().year - sum([worker.year for worker in staff]) / len(staff)
