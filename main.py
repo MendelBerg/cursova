e3473c72dbd82ac615e28a83c265ce370641c57f
Alex <mendelberg44@gmail.com>
2021-03-04 15:30:08 +0200

    code has refactored

--- main.py
+++ main.py
@@ -3,40 +3,7 @@ from collections import namedtuple
 Notes = namedtuple("Notes", "code name birth_year position unit exp money")
 
 
-# def put_data():
-#     a = []
-#     for x in range(int(input('Amount of staff: '))):
-#         b = []
-#         print(f'The person #{x + 1}:')
-#         a.append(b.append(input('Code: '))),
-#         a.append(b.append(input('Name: '))),
-#         a.append(b.append(input('Birth year: '))),
-#         a.append(b.append(input('Position: '))),
-#         a.append(b.append(input('Unit: '))),
-#         a.append(b.append(input('Experience: '))),
-#         a.append(b.append(input('Money: ')))
-#         print()
-#     with open(f"data/notes.txt", "a") as file:
-#         for x in a:
-#             file.write(', '.join(x))...
-
-
-# def input_note():
-#     put_data([
-#                 Notes(
-#                     input(f'\nThe person #{_ + 1}\nCode: '),
-#                     input('Name: '),
-#                     input('Birth year: '),
-#                     input('Position: '),
-#                     input('Unit: '),
-#                     input('Experience: '),
-#                     input('Money: ')
-#                 )
-#                 for _ in range(
-#                     int(input('Amount of staff: '))
-#                 )
-#             ])
-
+# put_data(get_input_notes_arr())
 def put_data(staff):
     with open(f"data/notes.txt", "w") as file:
         for worker in staff:
@@ -46,8 +13,8 @@ def put_data(staff):
             )
 
 
-def input_note():
-    put_data([
+def get_input_notes_arr():
+    return [
         Notes(
             input(f'\nThe person #{_ + 1}\nCode: '),
             input('Name: '),
@@ -60,7 +27,7 @@ def input_note():
         for _ in range(
             int(input('Amount of staff: '))
         )
-    ])
+    ]
 
 
 def get_arr_notes():
@@ -76,11 +43,13 @@ def get_arr_notes():
         ]
 
 
+# get_notes_filter('unit', get_arr_notes())
 def get_notes_filter(unit, staff):
     workers = [worker for worker in staff if worker.unit == unit]
     return workers if len(workers) != 0 else 0
 
 
+# show_notes_filter(get_notes_filter('unit', get_arr_notes()))
 def show_notes_filter(filtered_notes):
     if filtered_notes:
         print(*[worker.name for worker in filtered_notes], sep='\n')
@@ -88,19 +57,18 @@ def show_notes_filter(filtered_notes):
         print('Error!\nThis unit does not exists.')
 
 
+# get_middle_age_staff(get_notes_filter('unit', get_arr_notes()))
 def get_middle_age_staff(staff):
     from datetime import datetime as date
-    print(date.now().year - round(sum(
-        [
-            worker.birth_year for worker in staff
-        ]) / len(staff))
-          )
+    print(
+        date.now().year - round(
+            sum([worker.birth_year for worker in staff]) / len(staff)
+        )
+    )
 
 
-# get_middle_age_staff(get_notes_filter('a', get_arr_notes()))
-
-
-def small_exp(staff):
+# show_small_exp(get_arr_notes())
+def show_small_exp(staff):
     for bypass in range(1, len(staff)):
         for i in range(len(staff) - bypass):
             if staff[i].exp > staff[i + 1].exp:
@@ -108,6 +76,3 @@ def small_exp(staff):
 
     for x in staff[:int(len(staff) * .5)]:
         print(x.exp)
-
-
-# small_exp(get_arr_notes())
