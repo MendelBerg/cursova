8b802450baa1f82f8d8c504ef316455848bb589c
Alex <mendelberg44@gmail.com>
2021-04-09 23:00:35 +0300

    main page

--- Interfaces/menu/btns_func.py
+++ Interfaces/menu/btns_func.py
@@ -5,7 +5,8 @@ import Interfaces.content.middle_age as m
 
 
 def btn_click_main():
-    window['bg'] = 'green'
+    content = c.create_frame_content()
+    content['text'] = 'Дані про працівників ТОВ "Снігова корова"'
 
 
 def btn_click_put():
@@ -17,26 +18,24 @@ def btn_click_staff():
 
 
 def btn_click_age():
-    window['bg'] = 'yellow'
     content = c.create_frame_content()
     text_arr = []
     for unit, age in m.show_middle_age():
         year = 'рік' if age % 10 == 1 \
             else 'роки' if age % 10 == 2 or age % 10 == 3 or age % 10 == 4 \
             else 'років'
-        text_arr.append(f'Підрозділ {unit}, {age} {year}.\n')
+        text_arr.append(f'Підрозділ "{unit.capitalize()}", {age} {year}.\n')
 
     content['text'] = ''.join(text_arr).strip()
 
 
 def btn_click_exp():
-    window['bg'] = 'pink'
     content = c.create_frame_content()
     text_arr = []
     for worker in e.show_small_exp():
         year = 'рік' if worker.exp % 10 == 1 \
             else 'роки' if worker.exp % 10 == 2 or worker.exp % 10 == 3 or worker.exp % 10 == 4 \
             else 'років'
-        text_arr.append(f'Працівник {worker.name}, підрозділ {worker.unit}, стаж {worker.exp} {year}.\n')
+        text_arr.append(f'Працівник {worker.name}, підрозділ "{worker.unit.capitalize()}", стаж {worker.exp} {year}.\n')
 
     content['text'] = ''.join(text_arr).strip()
