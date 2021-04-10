fb5c5012272faec874699633a0bf5e52206153bb
Alex <mendelberg44@gmail.com>
2021-04-10 17:44:32 +0300

    add end year

--- Interfaces/menu/btns_func.py
+++ Interfaces/menu/btns_func.py
@@ -24,21 +24,25 @@ def btn_click_age():
     content = t.create_frame_content()[0]
     text_arr = []
     for unit, age in m.show_middle_age():
-        year = 'рік' if age % 10 == 1 and age != 11 \
-            else 'роки' if age % 10 == 2 or age % 10 == 3 or age % 10 == 4 \
-            else 'років'
+        year = add_end_year(age)
         text_arr.append(f'Підрозділ "{unit.capitalize()}", {age} {year}.\n')
 
     content['text'] = ''.join(text_arr).strip()
+
+
+def add_end_year(num):
+    return 'рік' if num % 10 == 1 and num != 11 \
+        else 'роки' if num % 10 == 2 and num != 12 or \
+                       num % 10 == 3 and num != 13 or \
+                       num % 10 == 4 and num != 14 \
+        else 'років'
 
 
 def btn_click_exp():
     content = t.create_frame_content()[0]
     text_arr = []
     for worker in e.show_small_exp():
-        year = 'рік' if worker.exp % 10 == 1 \
-            else 'роки' if worker.exp % 10 == 2 or worker.exp % 10 == 3 or worker.exp % 10 == 4 \
-            else 'років'
+        year = add_end_year(worker.exp)
         text_arr.append(f'Працівник {worker.name}, підрозділ "{worker.unit.capitalize()}", стаж {worker.exp} {year}.\n')
 
     content['text'] = ''.join(text_arr).strip()
