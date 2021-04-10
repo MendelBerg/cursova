e2f5a83397f9e920cd7037a58ebb35c654e13815
Alex <mendelberg44@gmail.com>
2021-04-10 09:32:06 +0300

    title in staff list

--- Interfaces/content/staff_list.py
+++ Interfaces/content/staff_list.py
@@ -29,7 +29,10 @@ def get_units(staff):
 
 def show_select():
     menu2 = Frame()
-    content = Label(menu2, text="Choose your favourite month")
+    content = Label(menu2, text='Choose unit')
+    content.pack()
+
+
     menu2.place(relx=0.24, rely=0, relwidth=0.76, relheight=1)
     menu2['bg'] = 'red'
     menu2['width'] = '500'
