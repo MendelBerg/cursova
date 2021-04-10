8bb15797e4060c6a07c69b0bcb12573af9c2a4f5
Alex <mendelberg44@gmail.com>
2021-04-10 09:01:52 +0300

    get sorted units

--- Interfaces/content/staff_list.py
+++ Interfaces/content/staff_list.py
@@ -1,5 +1,32 @@
 from Interfaces.init import *
+from tools import *
 
+
+def get_arr_notes(func):
+    def wrapper():
+        with open('../data/notes.txt', 'r') as file:
+            return func([
+                Notes(
+                    *[int(y) if y.strip().isdigit() else y for y in x.split(', ')]
+                ) for x in file
+            ]
+            )
+    return wrapper
+
+
+def sort_data(staff):
+    for top in range(1, len(staff)):
+        i = top
+        while i > 0 and staff[i-1] > staff[i]:
+            staff[i], staff[i - 1] = staff[i - 1], staff[i]
+            i -= 1
+
+    return staff
+
+@get_arr_notes
+def get_units(staff):
+    units = sort_data([*set([x.unit for x in staff])])
+    print(units)
 
 def show_select(frame):
     menu2 = Frame()
@@ -9,8 +36,8 @@ def show_select(frame):
     menu2['width'] = '500'
 
     def callbackFunc():
-        myLabel = Label(menu2, text=clicked.get()).pack()
-
+        # myLabel = Label(menu2, text=clicked.get()).pack()
+        get_units()
     options = [
         'a',
         'b',
@@ -18,7 +45,7 @@ def show_select(frame):
     ]
 
     clicked = StringVar()
-    clicked.set(options[1])
+    clicked.set(options[0])
 
     drop = OptionMenu(menu2, clicked, *options)
     drop.pack(pady=20)
