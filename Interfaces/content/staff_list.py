77d65c9faa51b7de52b0958ff492890efa8bd239
Alex <mendelberg44@gmail.com>
2021-04-10 08:52:20 +0300

    pattern for select unit

--- Interfaces/content/staff_list.py
+++ Interfaces/content/staff_list.py
@@ -1,15 +1,15 @@
-from tkinter.ttk import Combobox
-
-from tools import *
 from Interfaces.init import *
 
 
 def show_select(frame):
     menu2 = Frame()
-    content = Label(menu2)
+    content = Label(menu2, text="Choose your favourite month")
     menu2.place(relx=0.24, rely=0, relwidth=0.76, relheight=1)
     menu2['bg'] = 'red'
     menu2['width'] = '500'
+
+    def callbackFunc():
+        myLabel = Label(menu2, text=clicked.get()).pack()
 
     options = [
         'a',
@@ -23,6 +23,6 @@ def show_select(frame):
     drop = OptionMenu(menu2, clicked, *options)
     drop.pack(pady=20)
 
+    btn = Button(menu2, text='select', command=callbackFunc)
+    btn.pack(pady=20)
 
-# def callbackFunc(event):
-#     print("New Element Selected")
