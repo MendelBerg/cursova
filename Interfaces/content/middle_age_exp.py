efd78014395b96ebc817a095b04613e800396e92
Alex <mendelberg44@gmail.com>
2021-04-11 08:14:53 +0300

    fill content in middle age exp

--- Interfaces/content/middle_age_exp.py
+++ Interfaces/content/middle_age_exp.py
@@ -15,7 +15,7 @@ def show_small_exp():
         min_exp_staff.append({'name': worker.name, 'unit': worker.unit, 'years': worker.exp})
         min_exp = 0
 
-    return sort_dict(min_exp_staff, 'name')
+    return sort_dict_arr(min_exp_staff, 'name')
 
 
 def show_middle_age():
@@ -26,18 +26,39 @@ def show_middle_age():
         workers = filter_by_unit(unit)
         middle_age_arr.append({
             'unit': unit,
-            "years": date.now().year - round(
+            'years': date.now().year - round(
                 sum([worker.birth_year for worker in workers]) / len(workers)
             )
         })
 
-    return sort_dict(middle_age_arr, 'unit')
+    return sort_dict_arr(middle_age_arr, 'unit')
 
 
-def sort_dict(staff, key):
+def sort_dict_arr(staff, key):
     for top in range(1, len(staff)):
         i = top
         while i > 0 and staff[i - 1][key] > staff[i][key]:
             staff[i], staff[i - 1] = staff[i - 1], staff[i]
             i -= 1
     return staff
+
+
+def fill_content(title):
+    data = show_middle_age() if title == 'середній вік' else show_small_exp()
+    text_arr = []
+    for worker in data:
+        year = add_end_year(worker['years'])
+        text_arr.append(f'Підрозділ "{worker["unit"]}", {worker["years"]} {year}.\n'
+                        if len(data[0]) == 2 else
+                        f'Працівник {worker["name"]}, підрозділ "{worker["unit"]}", стаж {worker["years"]} {year}.\n '
+                        )
+
+    return f"Данні про {title} працівників\n\n{''.join(text_arr).strip()}"
+
+
+def add_end_year(num):
+    return 'рік' if num % 10 == 1 and num != 11 \
+        else 'роки' if num % 10 == 2 and num != 12 or \
+                       num % 10 == 3 and num != 13 or \
+                       num % 10 == 4 and num != 14 \
+        else 'років'
--- Interfaces/menu/btns_func.py
+++ Interfaces/menu/btns_func.py
@@ -18,32 +18,10 @@ def btn_click_staff():
 
 
 def btn_click_age():
-    from Interfaces.content.middle_age_exp import show_middle_age
-    fill_content(show_middle_age(), 'середній вік')
+    from Interfaces.content.middle_age_exp import fill_content
+    create_frame_content(fill_content('середній вік'))
 
 
 def btn_click_exp():
-    from Interfaces.content.middle_age_exp import show_small_exp
-    fill_content(show_small_exp(), 'найменший досвід')
-
-
-def fill_content(data, title):
-    label = create_frame_content('')[0]
-    text_arr = []
-
-    for worker in data:
-        year = add_end_year(worker['years'])
-        text_arr.append(f'Підрозділ "{worker["unit"].capitalize()}", {worker["years"]} {year}.\n'
-                        if len(data[0]) == 2 else
-                        f'Працівник {worker["name"]}, підрозділ "{worker["unit"].capitalize()}", стаж {worker["years"]} {year}.\n '
-                        )
-
-    label['text'] = f"Данні про {title} працівників\n\n{''.join(text_arr).strip()}"
-
-
-def add_end_year(num):
-    return 'рік' if num % 10 == 1 and num != 11 \
-        else 'роки' if num % 10 == 2 and num != 12 or \
-                       num % 10 == 3 and num != 13 or \
-                       num % 10 == 4 and num != 14 \
-        else 'років'
+    from Interfaces.content.middle_age_exp import fill_content
+    create_frame_content(fill_content('найменший досвід'))
