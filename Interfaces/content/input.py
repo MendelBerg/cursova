345df36ba617851eb996e8d5d905868fff5b5cf9
Alex <mendelberg44@gmail.com>
2021-04-17 09:50:31 +0300

    show errors

--- Interfaces/content/input.py
+++ Interfaces/content/input.py
@@ -1,14 +1,17 @@
 from tkinter import Entry, messagebox, LEFT, RIGHT
 from tools import create_btn, create_label
 
+labels = ["Код", "ПІБ", "Рік народження", "Посада",
+          "Підрозділ", "Досвід роботи (в роках)", "Зарплатня"]
+
 
 def find_errors(entries_arr):
     notes = [e.get() for e in entries_arr]
     errors_messages = {}
     dict_errors = {
-        'empty': 'поле повинно бути заповнене!',
-        'digits': 'Код повинен складатися лише з чисел!',
-        'len': 'Код повинен складатися з чотирьох чисел!',
+        'empty': 'Поле повинно бути заповнене!',
+        'digits': 'Поле повинно складатися лише з чисел!',
+        'len': 'Поле повинно складатися з чотирьох чисел!',
         'alphabetic': 'Поле має скаладтися лише з літер',
         'full name': 'Ви повинні ввести ПІБ',
         'full fname': 'Ім\'я повинне бути не менше 2-х симовлів',
@@ -16,51 +19,52 @@ def find_errors(entries_arr):
         'full father_name': 'По-батькові повинне бути на менше 6-ти символів'
     }
 
-    for i in dict_errors:
+    for i in range(len(notes)):
         errors_messages[i] = []
 
-    for note in notes:
+    for note in range(len(notes)):
         if notes[note] == '':
-            errors_messages['empty'].append(note)
+            errors_messages[note].append('empty')
 
     for note in range(len(notes)):
         if notes[note] != '':
             if note == 0 or note == 2 or note == 5 or note == 6:
                 if not notes[note].isdigit():
-                    errors_messages['digits'].append(note)
+                    errors_messages[note].append('digits')
             if note == 0 or note == 2:
-                if not notes[note] != 4:
-                    errors_messages['len'].append(note)
+                if len(notes[note]) != 4:
+                    errors_messages[note].append('len')
             if note == 1 or note == 3 or note == 4:
                 if not notes[note].replace(' ', '').isalpha():
-                    errors_messages['alphabetic'].append(note)
+                    errors_messages[note].append('alphabetic')
 
     if notes[1] != '':
         count = [len(x) for x in notes[1].strip().split(' ')]
 
         if len(notes[1].strip().split(' ')) != 3:
-            errors_messages['full name'].append(1)
-        else:
+            errors_messages[1].append('full name')
+        elif notes[1].replace(' ', '').isalpha():
             if count[0] < 2:
-                errors_messages['full lname'].append(1)
+                errors_messages[1].append('full lname')
             if count[1] < 2:
-                errors_messages['full fname'].append(1)
+                errors_messages[1].append('full fname')
             if count[2] < 6:
-                errors_messages['full father_name'].append(1)
+                errors_messages[1].append('full father_name')
 
-    # Зберегти всі повідомлення в одну змінну і вивести їх в одному  messagebox.showwarning під параметром message
-    # НАЗВА УСІХ ПОЛІВ, ЯКІ ВІДНОСЯТЬСЯ ДО ПОМИЛКИ => ПОВІДОМЛЕННЯ
-    # errors_text = ''
-    # for error in errors_messages:
-    #     if errors_messages[error]:
-    #         for x in
+    errors_text = ''
+    for field in errors_messages:
+        if errors_messages[field]:
+            errors_text += f'{labels[field]}:' + '\n' + \
+                           '\n'.join([dict_errors[error] for error in errors_messages[field]]) + \
+                           '\n\n'
+
+    return messagebox.showwarning(title='Обережно!', message=errors_text) if errors_text else False
+
 
 def put_data(entries_arr):
     notes = [e.get() for e in entries_arr]
-    find_errors(entries_arr)
-    if '' in notes:
-        messagebox.showwarning(title='Обережно!', message='Ви заповнили не всі поля!')
-    else:
+
+    if not find_errors(entries_arr):
         with open(f"../data/notes.txt", "a") as file:
             file.write(', '.join(notes).strip(', ') + '\n')
 
@@ -76,9 +80,6 @@ def clear_data(entries_arr):
 
 
 def create_input(frame):
-    labels = ["Код:", "ПІБ:", "Рік народження:", "Посада:",
-              "Підрозділ", "Досвід роботи (в роках)", "Зарплатня"]
-
     entries_arr = []
     for text in labels:
         create_label(frame, text, size=11).pack(pady=5)
