d4e698408447aa755c5dcbccd0a2e7680fd45ae6
Alex <mendelberg44@gmail.com>
2021-04-20 09:51:53 +0300

    show all staff

--- Interfaces/content/struct.py
+++ Interfaces/content/struct.py
@@ -5,26 +5,30 @@ labels = ["Код", "ПІБ", "Рік народження", "Посада",
 
 
 def get_struct():
-    arr = []
+    dict_text = {}
     i = 0
     for person in sort_dict_arr(get_arr_notes('../'), 0):
+        dict_text[i] = []
         for data in range(len(person)):
-            arr.append(f'{labels[data]}: {person[data]}')
+            dict_text[i].append(f'{labels[data]}: {person[data]}')
         i += 1
-    return arr
+    return dict_text
 
 
 def show_struct():
     root = Tk()
     root.title("Список працівників")
     root.geometry('1110x700')
-    scrollbar = Scrollbar(root)
+    scrollbar = Scrollbar(root, bg='#1A2026')
     scrollbar.pack(side=RIGHT, fill=Y)
 
     my_list = Listbox(root, yscrollcommand=scrollbar.set, font=f"Georgia 20", bg=content_bg)
+    d = get_struct()
 
-    for line in get_struct():
-        my_list.insert(END, line)
+    for line in d:
+        for data in d[line]:
+            my_list.insert(END, data)
+        my_list.insert(END, '===========================')
 
     my_list.pack(side=LEFT, fill=BOTH, ipady=100, ipadx=1000)
     scrollbar.config(command=my_list.yview)
